820835a8e3bb1e01ca86dfb01907204fa5ad7de3
Cameron Brown <moontsukuyomi6@gmail.com>
2025-02-24 16:19:43 +0200

    Create operate function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+let num1 = '';
+let num2 = '';
+let operator = '';
+
 function add(num1, num2) {
   return Number(num1) + Number(num2);
 }
@@ -15,4 +19,17 @@ function divide(num1, num2) {
     return 'Cannot divide by 0';
   }
   return Number(num1) / Number(num2); 
+}
+
+function operate(operator, num1, num2) {
+  switch(operator) {
+    case '+':
+      return add(num1, num2);
+    case '-':
+      return subtract(num1, num2);
+    case '*':
+      return multiply(num1, num2);
+    case '/':
+      return divide(num1, num2);
+  }
 }
